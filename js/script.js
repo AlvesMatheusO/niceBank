356a1cecd5b9fda8a6d0677d6aba132f3175b6b0
Matheus Alves <matheusalv.oliveira@gmail.com>
2023-04-26 17:00:24 -0300

    cpf validation set of identical numbers and taking off expressions

---
+++ js/script.js
@@ -0,0 +1,15 @@
+import isCPF from './valida-cpf.js';
+
+const camposDoForm = document.querySelectorAll("[required]");
+
+camposDoForm.forEach((campo) => {
+    campo.addEventListener("blur", () => verificaCampo(campo));
+});
+
+function verificaCampo(campo) {
+    
+    if (campo.name == "cpf" && campo.value.length >= 11) {
+        isCPF(campo);
+    }
+
+}
